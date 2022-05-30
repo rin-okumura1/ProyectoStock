37ceb14de75517fccc13498fe2c95eececef69ca
LUCIO GARCIA <luciogarcialam@gmail.com>
2022-05-30 00:03:53 -0300

    Hola

--- ProyectoStock/Startup.cs
+++ ProyectoStock/Startup.cs
@@ -25,8 +25,8 @@ namespace ProyectoStock
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            services.AddDbContext<BaseDeDatos>(options => options.UseSqlite(@"filename=C:\Users\david\Desktop\ProyectoStock\ProyectoStock\BaseDeDatosProyecto.db"));
+            services.AddControllersWithViews(); 
+            services.AddDbContext<BaseDeDatos>(options => options.UseSqlite(@"filename=E:\MARIO\ProyectoStock\ProyectoStock\BaseDeDatosProyecto.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
